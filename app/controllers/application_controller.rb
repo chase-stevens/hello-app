e3e8fe1fa83a362308258cbf1a7a7bca2ff2347d
chase-stevens <chasestevenspersonal@gmail.com>
2018-07-27 09:32:13 -0400

    Fixed landing page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def hello
-    render html: "hello, world!"
-    render html: "this is chase's first heroku based app"
+    render html: "hello, world! this is chase's first heroku based app"
   end
 end
